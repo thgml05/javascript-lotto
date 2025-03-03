50a0f89994032df86e7b842d90d9ac7071f0a0f3
thgml05 <aisohii@naver.com>
2025-03-03 12:20:44 +0900

    Updates

--- assets/index-arH8G1Y5.js
+++ assets/index-arH8G1Y5.js
@@ -343,8 +343,7 @@ const state = {
   winningNumbers: []
 };
 document.addEventListener("DOMContentLoaded", () => {
-  const purchaseButton = document.querySelector("#purchase-button");
-  purchaseButton.addEventListener("click", purchase);
+  elements.purchaseButton.addEventListener("click", purchase);
   elements.resultButton.addEventListener("click", checkResult);
   elements.modalCloseButton.addEventListener("click", closeModal);
   elements.restartButton.addEventListener("click", restartLotto);
